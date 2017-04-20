1c51d54e3a6ffe6f305bc1482421de66682de41a
PrabjotLattar <prabjot.laddar@gmail.com>
2017-04-20 00:21:08 -0300

    Add files via upload
    Code First approach to create Database

---
+++ TrackerDbContext.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrackerDbModel
+{
+    /// <summary>
+    /// Db Context class to set up the database and set up communication to the database
+    /// </summary>
+    public class TrackerDbContext : DbContext
+    {
+        /// <summary>
+        /// Assigning TimeTrackDB name to the database
+        /// </summary>
+        public TrackerDbContext() : base("TimeTrackDb")
+        {
+            //Database.SetInitializer(new DropCreateDatabaseIfModelChanges<TrackerDbContext>());
+        }
+
+
+        public DbSet<Absent> Absents { get; set; }
+        public DbSet<Campus> Campuses { get; set; }
+        public DbSet<Description> Descriptions { get; set; }
+
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<Message> Messages { get; set; }
+
+        public DbSet<Workhour> Workhours { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            //fluent api
+            //
+           
+
+        }
+
+
+
+    }
+}
